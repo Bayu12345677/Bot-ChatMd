efd8c625f1f175ec2e66d3404a005b60db15d93b
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2022-08-27 09:46:00 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ source <(cat lib/moduler.sh)
 Namespace Std :: Sys;{
 	{ @require: __main__.bot; };{ eval shell.ai; }
 };
-	eval Assistent.config Intents="config.json" Debug=False
+	eval Assistent.config Intents="https://raw.githubusercontent.com/Bayu12345677/Bot-ChatMd/master/config.json" Debug=False
 
 until false; do {
 	read -p "You : " input
